a5f557ad4d2fa75aa3492032c703866be756b880
leonidig <154411982+leonidig@users.noreply.github.com>
2024-02-06 23:33:41 +0200

    add new func

--- game_data/main.py
+++ game_data/main.py
@@ -2,29 +2,6 @@ import random
 from personages import *
 
 
-class Monster:
-    def __init__(self, Htype, hp, mana, strength, agility, 
-                 intelligense, xp, gold_range) :
-        self.Htype = Htype
-        self.hp = hp
-        self.mana = mana
-        self.strength = strength
-        self.agility = agility
-        self.intelligense = intelligense
-        self.xp = xp
-        self.gold = random.randint(*gold_range)
-    
-
-class Hero:
-    def __init__(self, name, gender, hp, mana, strength, agility, intelligense, Htype):
-        self.name = name
-        self.gender = gender
-        self.hp = hp
-        self.mana = mana
-        self.strength = strength
-        self.agility = agility
-        self.intelligense = intelligense
-        self.Htype = Htype
 
 heroes = {
     'V': Hero('Валькирия', 'Д', 40, 17, 20, 30, 35, 'Маг - Чародей'),
@@ -48,38 +25,47 @@ print('''\033[0;43mВыберите Персонажа:\033[0m
 • P - Папанистер
 ''')
 
+
+
+monster = random.choice(all_monstres)
 u_input = input("Выберите Персонажа: ").upper()
 
 if u_input in heroes:
     selected_hero = heroes[u_input]
-    print(f"Выбран персонаж: {selected_hero.name}")
+    print(f"Выбран персонаж: {selected_hero.name}\nВраг - {monster.Htype}")
 
 
 
-while selected_hero.hp > 0 and monster_1.hp > 0:
+while selected_hero.hp > 0 and monster.hp > 0:
     
     input(selected_hero.name + ', Нажми ENTER Для Атаки\n')
 
 
     hero_damage = random.randint(0, selected_hero.strength)
-    monster_1.hp -= hero_damage
+    monster.hp -= hero_damage
     print(f'{selected_hero.name} Наносит Урон : {hero_damage}')
 
 
-    monster_damage = random.randint(0, monster_1.strength)
+    monster_damage = random.randint(0, monster.strength)
     selected_hero.hp -= monster_damage
-    print(f'{monster_1.Htype} Наносит Ответный Удар: {monster_damage} урона\n{selected_hero.name}: Оcталось {selected_hero.hp} HP')
+    print(f'{monster.Htype} Наносит Ответный Удар: {monster_damage} урона\n{selected_hero.name}: Оcталось {selected_hero.hp} HP')
     
-    if monster_1.hp > 0:
-        print(f'{monster_1.Htype}: Осталось {monster_1.hp} HP\n')
+    if monster.hp > 0:
+        print(f'{monster.Htype}: Осталось {monster.hp} HP\n')
 
 
 
 if monster_damage == 0:
-    print(monster_1.Htype + ' Промахнулся\n')
+    print(monster.Htype + ' Промахнулся\n')
 
-elif hero_damage == 0:
+
+if hero_damage == 0:
     print(selected_hero.name + ' Промахнулся\n')
 
-if selected_hero.hp > 0 and monster_1.hp <= 0:
-        print(f'{selected_hero.name} Убил {monster_1.Htype}\n')
+
+if selected_hero.hp > 0 and monster.hp <= 0:
+        print(f'{selected_hero.name} Убил {monster.Htype}\n')
+
+
+if monster.hp > 0 and selected_hero.hp <= 0:
+        print(f'{selected_hero.name} Убил {monster.Htype}\n')
