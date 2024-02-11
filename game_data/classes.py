19b162d24eb4d19be4acb6def0d7cbb893b7be0d
leonidig <154411982+leonidig@users.noreply.github.com>
2024-02-11 23:01:37 +0200

    Now the game is fair.

--- game_data/classes.py
+++ game_data/classes.py
@@ -2,15 +2,13 @@ import random
 
 class Hero:
 
-    def __init__(self, name, gender, hp, mana, strength, agility, 
-                 intelligense, Htype=None) :
+    def __init__(self, name, hp, mana, strength, agility, 
+                 Htype=None) :
         self.name = name
-        self.gender = gender
         self.hp = hp
         self.mana = mana
         self.strength = strength
         self.agility = agility
-        self.intelligense = intelligense
         self.Htype = Htype
         self.xp = 0
         self.gold = 0
@@ -18,13 +16,12 @@ class Hero:
 
 class Monster:
     def __init__(self, Htype, hp, mana, strength, agility, 
-                 intelligense, xp, gold_range) :
+                 xp, gold_range) :
         self.Htype = Htype
         self.hp = hp
         self.mana = mana
         self.strength = strength
         self.agility = agility
-        self.intelligense = intelligense
         self.xp = xp
         self.gold = random.randint(*gold_range)
     
--- game_data/main.py
+++ game_data/main.py
@@ -2,70 +2,72 @@ import random
 from personages import *
 
 
-
 heroes = {
-    'V': Hero('Валькирия', 'Д', 40, 17, 20, 30, 35, 'Маг - Чародей'),
-    'O': Hero('Одрикс', 'М', 70, 3, 50, 10, 20, 'Танк'),
-    'P': Hero('Папанистер', 'М', 55, 10, 20, 35, 37, 'Всадник')
+    'V': Hero('Валькирия', 40, 17, 20, 30, 'Маг - Чародей'),
+    'O': Hero('Одрикс', 70, 3, 50, 10,'Танк'),
+    'P': Hero('Папанистер', 55, 10, 20, 35, 'Всадник'),
+    'D': Hero('Довбик', 50, 10, 30, 15,'Доблестный Пон')
 }
 
 for hero_key, hero_value in heroes.items():
     print(f'''\033[0;41m\033[0;31mИмя - {hero_value.name}\033[0m,
-• Гендер - {hero_value.gender},
 • Здоровье - {hero_value.hp},
 • Мана - {hero_value.mana},
 • Cила - {hero_value.strength},
 • Ловкость - {hero_value.agility},
-• Интеллект - {hero_value.intelligense},
 • Тип - {hero_value.Htype}\n''')
 
 print('''\033[0;43mВыберите Персонажа:\033[0m
 • V - Валькирия
 • О - Одрикс
 • P - Папанистер
+• D - Довбик  
 ''')
 
-
-
 monster = random.choice(all_monstres)
-u_input = input("Выберите Персонажа: ").upper()
+selected_hero = None
 
-if u_input in heroes:
-    selected_hero = heroes[u_input]
-    print(f"Выбран персонаж: {selected_hero.name}\nВраг - {monster.Htype}")
+while selected_hero is None:
+    u_input = input("Выберите Персонажа: ").upper()
+    if u_input in heroes:
+        selected_hero = heroes[u_input]
+        print(f"\033[0;43mВыбран персонаж: {selected_hero.name}\nВраг - {monster.Htype}\033[0m")
+    else:
+        print("Некорректный выбор персонажа. Попробуйте еще раз.")
 
+# Балансировка боя
+if 0 < monster.hp <= 20 and 40 < selected_hero.hp >= 50:
+    quantity = selected_hero.hp / monster.hp
+    total_m_hp = quantity * monster.hp
+    monster.hp = total_m_hp
+    total_m_strength = quantity * monster.strength
+    monster.strength = total_m_strength
+
+    print(f'Сбалансирован бой: количество монстров - {quantity}, общее HP монстров - {total_m_hp}, общая сила монстров - {total_m_strength}')
 
 
 while selected_hero.hp > 0 and monster.hp > 0:
-    
     input(selected_hero.name + ', Нажми ENTER Для Атаки\n')
 
-
     hero_damage = random.randint(0, selected_hero.strength)
-    monster.hp -= hero_damage
-    print(f'{selected_hero.name} Наносит Урон : {hero_damage}')
+    if hero_damage <= 0:
+        print(f'{selected_hero.name} промахнулся!')
+    else:
+        monster.hp -= hero_damage
+        print(f'{selected_hero.name} Наносит Урон: {hero_damage}')
+        print(f'Осталось HP у монстра: {monster.hp}')  # Добавлен вывод HP монстра после урона
 
+        if monster.hp <= 0:
+            print(f'{selected_hero.name} Убил {monster.Htype}\n')
+            break
 
-    monster_damage = random.randint(0, monster.strength)
-    selected_hero.hp -= monster_damage
-    print(f'{monster.Htype} Наносит Ответный Удар: {monster_damage} урона\n{selected_hero.name}: Оcталось {selected_hero.hp} HP')
-    
-    if monster.hp > 0:
-        print(f'{monster.Htype}: Осталось {monster.hp} HP\n')
+    monster_damage = random.randint(0, int(monster.strength))  # Преобразование к целому числу
+    if monster_damage <= 0:
+        print(f'{monster.Htype} промахнулся!')
+    else:
+        selected_hero.hp -= monster_damage
+        print(f'{monster.Htype} Наносит Ответный Удар: {monster_damage} урона\n{selected_hero.name}: Оcталось {selected_hero.hp} HP')
 
-
-
-if monster_damage == 0:
-    print(monster.Htype + ' Промахнулся\n')
-
-
-if hero_damage == 0:
-    print(selected_hero.name + ' Промахнулся\n')
-
-
-if selected_hero.hp > 0 and monster.hp <= 0:
-        print(f'{selected_hero.name} Убил {monster.Htype}\n')
-
-
-if monster.hp > 0 and selected_hero.hp <= 0:
-        print(f'{selected_hero.name} Убил {monster.Htype}\n')
+        if selected_hero.hp <= 0:
+            print(f'{monster.Htype} Убил {selected_hero.name}\n')
+            break
